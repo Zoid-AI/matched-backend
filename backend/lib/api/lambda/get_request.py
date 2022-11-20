525a1e269db9b49d866992da278adbe805465ec1
Long Tran <long-ht@outlook.com>
2022-11-20 09:43:40 -0700

    add get request

---
+++ backend/lib/api/lambda/get_request.py
@@ -0,0 +1,32 @@
+import boto3
+import json
+import os
+from boto3.dynamodb.conditions import Key
+
+def handler(event, context):
+
+    table_name = os.environ["TABLE_NAME"]
+
+    raw_query = event['rawQueryString']
+    query = []
+    for sub in raw_query.split('&'):
+        if '=' in sub:
+            query.append(map(str.strip, sub.split('=', 1)))
+    query_dict = dict(query)
+
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(table_name)
+
+    response = table.query(
+        KeyConditionExpression=Key('pk').eq('#USER_ID{}'.format(query_dict['id'])) & Key('sk').begins_with(
+            '#MATCH'))
+
+    if 'Items' not in response:
+        return {
+            'statusCode': 404,
+        }
+
+    return {
+        'statusCode': 200,
+        "body": json.dumps(response['Items'])
+    }
